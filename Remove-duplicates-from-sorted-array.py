152d3dc4799cef7ad3a3ab7680336bf52adf2871
mohamed meqlad <76888049+mohamedmm9@users.noreply.github.com>
2022-09-13 10:38:57 -0700

    Create Remove-duplicates-from-sorted-array.py

---
+++ Remove-duplicates-from-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        
+        count = 0
+       
+        for i in range(len(nums)):
+           
+            if i < len(nums) - 1 and nums[i] == nums[i + 1]:
+                continue
+            
+            nums[count] = nums[i]
+            count += 1
+        return count
